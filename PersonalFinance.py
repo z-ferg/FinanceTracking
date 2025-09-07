0e61a663ae95979db46f2ab474c3598359efd6b9
Zach Ferguson <zachferg04@gmail.com>
2025-09-07 16:11:58 -0400

    Good progress on supabase overhaul, lots of code cleanup to be done though.

--- PersonalFinance.py
+++ PersonalFinance.py
@@ -1,6 +1,6 @@
 import mysql.connector, os
 import sensitive
-import supabase_utils
+import supabase_utils as su
 from datetime import datetime
 
 def clear_and_print(header):
@@ -11,18 +11,8 @@ def clear_and_print(header):
     print(header)
 
 
-def open_sql():
-    my_db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password=sensitive.password,
-        database="Personal_Finance_DB"
-    )
-    return (my_db.cursor(), my_db)
-
-
 def add_transaction():
-    # First get the type of transaction, is it expense or income
+    # DETERMINE EXPENSE VS INCOME
     trans_type = -1
     while trans_type not in (1, 2):
         clear_and_print("-----Transaction Type-----")
@@ -30,117 +20,104 @@ def add_transaction():
         print("2) Income")
         trans_type = int(input("Type: "))
     
-    cursor, my_db = open_sql()
-    
-    if trans_type == 1:   
-        cat_table = cursor.execute("SELECT * FROM expense_categories")
-        expense = True
-    elif trans_type == 2: 
-        cat_table = cursor.execute("SELECT * FROM income_categories")
-        expense = False
-    
-    all_rows = cursor.fetchall()
-    max_val = len(all_rows)
+    expense = (True, False)[trans_type - 1]
+    valid_table = "PF_expense_categories" if expense else "PF_income_categories"
+
+    all_categories = su.view_table(valid_table).data
+    max_val = len(all_categories)
     category_id = float('inf')
-    
+
     while category_id not in range(1, max_val + 1):
-        header = "-----Expense Type-----" if trans_type == 1 else "-----Income Type-----"
+        header = "-----Expense Type-----" if expense else "-----Income Type-----"
         clear_and_print(header)
-        for id, type in all_rows:
-            print(f'{id}) {type}')
+
+        for row in all_categories:
+            print(f"{row['id']}) {row['name']}")
+
         try:
             category_id = int(input("Type: "))
         except ValueError:
             pass
     
-    category_id, category_name = all_rows[category_id - 1]
-    
-    # Now lets get the account information
+    # FETCH THE ACCOUNT
     account_id = 0
-    cursor.execute("SELECT * FROM Personal_Finance_DB.accounts")
-    all_rows = cursor.fetchall()
-    
-    while account_id not in range(1, len(all_rows) + 1):
+    all_accounts = su.view_table("PF_accounts").data
+
+    while account_id not in range(1, len(all_accounts) + 1):
         clear_and_print("-----Account Information-----")
-        for id, name, _, _ in all_rows:
-            print(f'{id}) {name}')
-        account_id = int(input("Account: "))
+        
+        for row in all_accounts:
+            print(f"{row['id']}) {row['name']}")
+        
+        try:
+            account_id = int(input("Account: "))
+        except ValueError:
+            pass
     
-    # Now lets get the final information required
-    # TODO - set up validity check for the date input
-    
+    # FETCH ADDITIONAL TRANSACTION INFORMATION
+    # TODO -> Potentially use a natural language API to make a date checker
     clear_and_print("-----Additional Information-----")
-    date_str = input("MM-DD-YYYY: ")
-    date = datetime.strptime(date_str, "%m-%d-%Y").date()
-    
+    date, amt, desc = None, None, None
+
     while(True):
         try:
-            amt = float(input("Amount: "))
-            desc = input("Description: ")
+            if not date:
+                date_str = input("MM-DD-YYYY: ")
+                date = datetime.strptime(date_str, "%m-%d-%Y").date()
+            if not amt: 
+                amt = float(input("Amount: "))
+            if not desc: 
+                desc = input("Description: ")
             break
         except ValueError:
             clear_and_print("-----Additional Information-----")
-            print(f'MM-DD-YYYY: {date_str}')
+            if date: 
+                print(f'MM-DD-YYYY: {date_str}')
+            if amt: 
+                print("Amount: ", amt)
+            if desc: 
+                print("Description: ", desc)
+    
+    # PREPARE TO EXECUTE INSERT ON DATABASE
+    trans_dict = {'id': len(su.view_table("PF_transactions", view_limit=float('inf')).data) + 1, 
+                  'date': date.isoformat(),
+                  'amount': amt,
+                  'description': desc,
+                  'expense': expense,
+                  'category_id': category_id,
+                  'account_id': account_id,
+                }
+    su.add_to_db("PF_transactions", trans_dict)
             
-    """
-    transaction table layout:
-        date -> DATE
-        amount -> DECIMAL
-        description -> VARCHAR
-        category_id -> INT
-        account_id -> INT
-        recur_id -> INT
-    """
-    cursor.execute('''INSERT INTO transactions (date, amount, description, expense, category_id, account_id) VALUES
-                        (%s, %s, %s, %s, %s, %s);''', (date, amt, desc, expense, category_id, account_id))
-    
-    my_db.commit()
-    
-    cursor.close()
-    my_db.close()
-    
     
 def check_balances():
-    cursor, my_db = open_sql()
-    cursor.execute("SELECT * FROM accounts")
-    all_accounts = cursor.fetchall()
+    all_accounts = su.view_table("PF_accounts").data
     
-    selected_account = [0]
-    while selected_account[0] not in range(1, len(all_accounts) + 1):
+    selected_account_id = 0
+    while selected_account_id not in range(1, len(all_accounts) + 1):
         clear_and_print("-----Account to Check-----")
         for account in all_accounts:
-            print(f'{account[0]}) {account[1]}')
+            print(f'{account["id"]}) {account["name"]}')
         try:
-            selected_account = all_accounts[int(input("Account: ")) - 1]
+            selected_account_id = all_accounts[int(input("Account: ")) - 1]['id']
         except (ValueError, IndexError):
             pass
     
-    clear_and_print(f'-----{selected_account[1]}-----')
-    print(f'Type of Account: {selected_account[2]}')
-    print(f'Account Balance: {selected_account[3]}\n')
-    
-    cursor.execute(f"""SELECT * FROM transactions 
-                        WHERE account_id = {selected_account[0]}
-                        ORDER BY date DESC
-                        LIMIT 5""")
-    recent_transactions = cursor.fetchall()
-    
-    if len(recent_transactions) > 0: print(f'-----Recent Transactions-----')
-    
-    for i in range(0, min(len(recent_transactions), 5)):
-        #transaction -> [id, date, amount, description, expense, cat_id, act_id, recur_id]
-        t = recent_transactions[i]
-        print(f'date: {t[1]}\namount: ${t[2]}\ntype: {"Expense" if t[4] else "Income"}')
-        
-        if t[4]:
-            cursor.execute("""SELECT * FROM expense_categories""")
-            expenses = cursor.fetchall()
-            print(f'category: {expenses[t[5]][1]}\n')
-        else:
-            cursor.execute("""SELECT * FROM income_categories""")
-            incomes = cursor.fetchall()
-            print(f'category: {incomes[t[5]][1]}\n')
+    selected_account = all_accounts[selected_account_id - 1]
 
+    clear_and_print(f'-----{selected_account["name"]}-----')
+    print(f'Type of Account: {selected_account["type"]}')
+    print(f'Account Balance: {selected_account["balance"]}\n')
+
+    filter = {'col': 'account_id', 'op': 'eq', 'x': selected_account_id}
+    recent_transactions = su.view_table(table="PF_transactions", view_limit=5, filter=filter).data
+
+    for rt in recent_transactions:
+        if rt['expense']:
+            print(f"-${rt['amount']} - {rt['date']}")
+        else:
+            print(f"${rt['amount']} - {rt['date']}")
+        print(f"{rt['description']}\n")
 
 def program_primary():
     clear_and_print("-----Personal Finance Tracker-----")
@@ -151,6 +128,6 @@ def program_primary():
         print("2) Check Account Balances")
         action = int(input("Option: "))
     if action == 1: add_transaction()
-    elif action == 2: check_balances_2()
+    elif action == 2: check_balances()
 
 program_primary()
--- supabase_utils.py
+++ supabase_utils.py
@@ -3,15 +3,15 @@ from supabase import create_client
 
 PROJ_URL = "https://ldqjmpsccofsdmmxdgyf.supabase.co"
 
-def view_table(table, view_limit=25):
+def view_table(table, view_limit=25, filter=None):
     supabase = create_client(PROJ_URL, sensitive.service_role_key)
-    return supabase.table(table).select("*").limit(view_limit).execute()
+
+    if not filter:
+        return supabase.table(table).select("*").limit(view_limit).execute()
+    else:
+        return supabase.table(table).select().filter(column=filter['col'], operator=filter['op'], criteria=filter['x']).execute()
 
 
 def add_to_db(table, row_vals):
     supabase = create_client(PROJ_URL, sensitive.service_role_key)
     return supabase.table(table).insert(row_vals).execute()
-
-
-print(view_table("PF_income_categories"))
-
